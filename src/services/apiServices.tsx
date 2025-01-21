9d60986457bea8a95c18fd840f2bedf54df27304
Shivam Kumar Nayak <snayak@datopic.com>
2025-01-21 14:24:51 +0530

    token change

--- src/services/apiServices.tsx
+++ src/services/apiServices.tsx
@@ -4,7 +4,7 @@ import {  TransferSpaceDocumentsResponse } from '../types/documentType';
 
 const API_URL = 'http://18.119.92.131:8001/documents/transfer_space_documents';
 const TOKEN =
-  'ClLCcYtlzLQn1SfM-Z2RrLAjQMAbjX9byqdZ7m_Iu0CE_E4gUrjExGcU1QQKthWfOO_mdQ_TK2BXW3O0i3hRER8CktDun7tJqRHP-oHV2hrUdF8SX7MxDqtnFdjxkblKj14ops9xKYPf3eIE51UvB3FqcHs7IGiJoAN0hx75DJmYPYV_NH3HQCLI48GOrpK0o9TlX-f-hYsUxDPyiPQocJ3cYrOY0BYAg-2jGoKKPE-HtZu3fMP8rXulKdOeEmt7woH62U9GAP-iTunQVedRSunvMg92prItU4qj5Gb_lObsRnrGsIPWx16C1RJyxZW8nuaVqcIFq5T1';
+  'eQkexx--8RwJxQ6Zlzo0EECo0wit_yDn7IS_4t0zZs4iur2VUMBi0tJdTGm9qGEGLEoUau15tGIVNNowuml86SzGbN22mQbU1Sx289vcSBhMYQZbw9QvO8z17_5p0KKnADzLadLyB1UcLfZtJ0iW37C19hIVgXE6xeVeqFuznbvfGa4vri6NVvwtL4mqBoA5SdKgaO-4XjO570SNpj0SV8dOBKvNEn8x73iSiJwdcMtZkuDw1NOm9C6z1X4UoncW2ZNUG_UBJ61JhY0zEn_6dnVKNkMcNF2gQfNYJCh6IbLt4Ikm7ZQQCZCxXJwhbmC3NV7ch6XXvoa0';
 
 
 // Function to call the API
